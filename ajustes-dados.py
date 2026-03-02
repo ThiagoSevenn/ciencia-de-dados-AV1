123aaf83d6e6d8444adeb72a1359b14f81934ed1
ThiagoSevenn <thiagonevessc@gmail.com>
2026-03-02 09:56:41 -0300

    Primeiro commit

---
+++ ajustes-dados.py
@@ -0,0 +1,11 @@
+## Aqui será passado um pente fino nos dados com a finalidade de filtrar algumas lacunas que estão despadronizadas, essa despadronização é o grande problema quando
+## o objetivo é estudar exatamente os padrões, analisando as métricas que seriam importantes para a melhora do desempenho empresarial.
+
+import pandas as pd
+
+# Leitura do dataset
+data = pd.read_csv('dataset\RECLAMEAQUI_PAODEACUCAR.csv')
+
+
+
+print(data)
